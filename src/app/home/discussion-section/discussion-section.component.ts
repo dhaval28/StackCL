1ff04e6b64100e9fe97489328ab9ec03f36fdfe8
dhaval28 <petiwaladhaval@gmail.com>
2019-05-23 15:48:20 +0530

    Components for Side panel selection

---
+++ src/app/home/discussion-section/discussion-section.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discussion-section',
+  templateUrl: './discussion-section.component.html',
+  styleUrls: ['./discussion-section.component.css']
+})
+export class DiscussionSectionComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home/qa-section/qa-section.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qa-section',
+  templateUrl: './qa-section.component.html',
+  styleUrls: ['./qa-section.component.css']
+})
+export class QASectionComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
