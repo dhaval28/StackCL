8dc612ac57d62d6e87448eb39c406d996cd87999
dhaval28 <petiwaladhaval@gmail.com>
2019-05-23 13:12:18 +0530

    Updated Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { SigninComponent } from './login-page/signin/signin.component';
-import { SignupComponent } from './login-page/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: LoginPageComponent }
 ];
 
